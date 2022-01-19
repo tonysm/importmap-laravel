e7f6504f16a06599eb96ef69be284d8d605cb879
Tony Messias <tonysm@hey.com>
2022-01-19 00:52:52 -0300

    Use importmed maps in tags component

--- src/View/Components/Tags.php
+++ src/View/Components/Tags.php
@@ -3,22 +3,19 @@
 namespace Tonysm\ImportmapLaravel\View\Components;
 
 use Illuminate\View\Component;
+use Tonysm\ImportmapLaravel\Facades\Importmap;
 
 class Tags extends Component
 {
+    public function __construct(public string $entrypoint)
+    {
+    }
+
     public function render()
     {
         return view("importmap::tags", [
-            'entrypoint' => 'app',
-            'importmaps' => [
-                'imports' => [
-                    'app' => asset('js/app.js'),
-                    'alpinejs' => 'https://ga.jspm.io/npm:alpinejs@3.8.1/dist/module.esm.js',
-                ],
-            ],
-            'preloadedModules' => [
-                'https://ga.jspm.io/npm:alpinejs@3.8.1/dist/module.esm.js',
-            ],
+            'importmaps' => Importmap::asArray('asset'),
+            'preloadedModules' => Importmap::preloadedModulePaths('asset'),
         ]);
     }
 }
