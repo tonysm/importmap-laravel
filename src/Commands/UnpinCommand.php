692dd756f84bdd4c4e605b889c690afd2ea298f3
Tony Messias <tonysm@hey.com>
2022-01-19 01:11:39 -0300

    Copy the unpin command over

--- src/Commands/UnpinCommand.php
+++ src/Commands/UnpinCommand.php
@@ -3,17 +3,62 @@
 namespace Tonysm\ImportmapLaravel\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
+use Tonysm\ImportmapLaravel\Packager;
 
 class UnpinCommand extends Command
 {
-    public $signature = 'importmap:unpin';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = '
+        importmap:unpin
+        {--from-env=production : The CDN environment}
+        {--download : If used, the dependency will be downloaded to be checked in under version control instead of relying on CDNs.}
+        {--from=jspm : The CDN.}
+        {packages*}
+    ';
 
-    public $description = 'Removes pinned JavaScript dependencies.';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Removes a pinned dependency.';
 
-    public function handle(): int
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(Packager $packager)
     {
-        $this->comment('All done');
+        $packages = Arr::wrap($this->argument('packages'));
 
-        return self::SUCCESS;
+        if ($imports = $packager->import($packages, $this->option('from-env'), $this->option('from'))) {
+            $imports->each(function (string $url, string $package) use ($packager) {
+                if ($packager->packaged($package)) {
+                    if ($this->option('download')) {
+                        $this->info(sprintf('Unpinning and removing "%s"', $package));
+                    } else {
+                        $this->info(sprintf('Unpinning "%s"', $package));
+                    }
+
+                    $packager->remove($package);
+                }
+            });
+
+            return self::SUCCESS;
+        }
+
+        $this->error(sprintf(
+            "Couldn't find any packages in %s on %s",
+            implode(', ', $packages),
+            $this->option('from'),
+        ));
+
+        return self::FAILURE;
     }
 }
