f386974789ae3d5c5f1cfc8a9dd13e921f3dbd6d
Tony Messias <tonysm@hey.com>
2022-01-18 23:19:23 -0300

    Can explicitly point dependencies to a path

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -7,8 +7,13 @@ beforeEach(function () {
     $this->map = new Importmap(rootPath: __DIR__ . '/stubs/');
 
     $this->map->pin("app");
+    $this->map->pin("editor", to: "js/rich_text.js");
 });
 
 it('local bin with inferred to', function () {
     expect(Arr::get($this->map->asArray('asset'), 'imports.app'))->toEqual(asset('js/app.js'));
+});
+
+it('local pin with explicit to', function () {
+    expect(Arr::get($this->map->asArray('asset'), 'imports.editor'))->toEqual(asset('js/rich_text.js'));
 });
