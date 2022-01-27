3b4fbb5a24da29fea084b8eb52836187b16a0684
Tony Messias <tonysm@hey.com>
2022-01-26 23:21:28 -0300

    Add method hints to the Importmap facade

--- src/Facades/Importmap.php
+++ src/Facades/Importmap.php
@@ -5,6 +5,11 @@ namespace Tonysm\ImportmapLaravel\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static void pin(string $name, ?string $to = null, bool $preload = false)
+ * @method static void pinAllFrom(string $dir, ?string $under = null, ?string $to = null, bool $preload = false)
+ * @method static array asArray(callable $assetResolver)
+ * @method static array preloadedModulePaths(callable $assetResolver)
+ *
  * @see \Tonysm\ImportmapLaravel\Importmap
  */
 class Importmap extends Facade
