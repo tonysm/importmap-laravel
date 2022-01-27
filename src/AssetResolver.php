a10bd8616643461a66e1722621981d9270ecb5ee
Tony Messias <tonysm@hey.com>
2022-01-27 00:02:13 -0300

    Removes local env check, since production doesnt use the passed resolver

--- src/AssetResolver.php
+++ src/AssetResolver.php
@@ -10,14 +10,6 @@ class AssetResolver
 {
     public function __invoke(string $fileRelativePath)
     {
-        // Production should be using the `importmap:optimize`, which
-        // already adds the file digest to the file name itself, so
-        // we don't have to worry about other environments here.
-
-        if (! App::environment('local')) {
-            return asset($fileRelativePath);
-        }
-
         if (! File::exists($absolutePath = Importmap::getRootPath() . '/resources/' . trim($fileRelativePath, '/'))) {
             return asset($fileRelativePath);
         }
