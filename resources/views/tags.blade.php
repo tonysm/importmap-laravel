565450659d61ad269f7ff027d376429d6fc8dfcd
Tony Messias <tonysm@hey.com>
2022-02-11 00:13:18 -0300

    Allow specifying a nonce for CSP

--- resources/views/tags.blade.php
+++ resources/views/tags.blade.php
@@ -1,15 +1,16 @@
 @foreach ($preloadedModules as $preloadedModule)
-    <link rel="modulepreload" href="{{ $preloadedModule }}">
+    <link rel="modulepreload" href="{{ $preloadedModule }}"@if ($nonce) nonce="{{ $nonce }}"@endif />
 @endforeach
 
 @if (config('importmap.use_shim'))
-<script async src="https://ga.jspm.io/npm:es-module-shims@1.3.6/dist/es-module-shims.js"></script>
+@if ($nonce) <script type="esms-options" nonce="{{ $nonce }}">{"nonce":"{{ $nonce }}"}</script> @endif
+<script async src="https://ga.jspm.io/npm:es-module-shims@1.3.6/dist/es-module-shims.js" data-turbo-track="reload"@if ($nonce) nonce="{{ $nonce }}"@endif></script>
 @endif
 
-<script type="importmap">
+<script type="importmap" data-turbo-track="reload"@if ($nonce) nonce="{{ $nonce }}" @endif>
 @json($importmaps)
 </script>
 
-<script type="module">
-    import '{{ $entrypoint }}';
+<script type="module" data-turbo-track="reload"@if ($nonce) nonce="{{ $nonce }}" @endif>
+    import {{ $entrypoint }} as '{{ $entrypoint }}';
 </script>
