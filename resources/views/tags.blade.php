53b830e88baee9e0c7c567da6ea4b9acff51847a
Tony Messias <tonysm@hey.com>
2022-02-12 21:52:37 -0300

    Tweak the position of the modules

--- resources/views/tags.blade.php
+++ resources/views/tags.blade.php
@@ -1,17 +1,14 @@
+<script type="importmap" data-turbo-track="reload"@if ($nonce) nonce="{{ $nonce }}" @endif>
+    @js($importmaps)
+</script>
+
 @foreach ($preloadedModules as $preloadedModule)
     <link rel="modulepreload" href="{{ $preloadedModule }}"@if ($nonce) nonce="{{ $nonce }}"@endif />
 @endforeach
 
 @if (config('importmap.use_shim'))
-<link rel="modulepreload" href="https://ga.jspm.io/npm:es-module-shims@1.3.6/dist/es-module-shims.js"@if ($nonce) nonce="{{ $nonce }}"@endif />
 @if ($nonce) <script type="esms-options" nonce="{{ $nonce }}">{"nonce":"{{ $nonce }}"}</script> @endif
 <script async src="https://ga.jspm.io/npm:es-module-shims@1.3.6/dist/es-module-shims.js" data-turbo-track="reload"@if ($nonce) nonce="{{ $nonce }}"@endif></script>
 @endif
 
-<script type="importmap" data-turbo-track="reload"@if ($nonce) nonce="{{ $nonce }}" @endif>
-@json($importmaps)
-</script>
-
-<script type="module" data-turbo-track="reload"@if ($nonce) nonce="{{ $nonce }}" @endif>
-    import {{ $entrypoint }} from '{{ $entrypoint }}';
-</script>
+<script type="module" data-turbo-track="reload"@if ($nonce) nonce="{{ $nonce }}" @endif>import '{{ $entrypoint }}';</script>
