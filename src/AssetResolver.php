b6c22d1f047715b1f47393dc55a59730397aa55a
Tony Messias <tonysm@hey.com>
2022-07-29 21:37:39 -0300

    Handle pinned files from the public folder

--- src/AssetResolver.php
+++ src/AssetResolver.php
@@ -9,6 +9,10 @@ class AssetResolver
 {
     public function __invoke(string $fileRelativePath)
     {
+        if (str_starts_with($fileRelativePath, 'vendor/') && File::exists($absolutePath = public_path($fileRelativePath))) {
+            return asset($fileRelativePath) . '?digest=' . (new FileDigest())($absolutePath);
+        }
+
         if (! File::exists($absolutePath = Importmap::getFileAbsolutePath('/resources/' . trim($fileRelativePath, '/')))) {
             return asset($fileRelativePath);
         }
