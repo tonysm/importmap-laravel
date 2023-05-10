ab9904c34ccc9b3d57973e9d2c2ca0183e27bacd
Tony Messias <tonysm@hey.com>
2023-05-09 23:04:42 -0300

    Bump the shim version to 1.7.2

--- config/importmap.php
+++ config/importmap.php
@@ -23,7 +23,7 @@ return [
      | since you may bump the shim version without having to upgrade.
      |
      */
-    'shim_version' => env('IMPORTMAP_SHIM_VERSION', '1.6.3'),
+    'shim_version' => env('IMPORTMAP_SHIM_VERSION', '1.7.2'),
 
     /*
      |------------------------------------------------------------------
